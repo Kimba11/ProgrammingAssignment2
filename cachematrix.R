e285bc85f69b48e41ea2bf7bbd77a327a70729ec
kwoelfel <kwoelfel@outlook.com>
2014-09-21 17:33:20 -0400

    This is my second revision to  programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
 ## These functions are intended to speed up the computation of matrix inversions
-## my minimizing the number of times the matrix inversion is computed.
+## by minimizing the number of times the matrix inversion is computed
 ## They take advantage of an R function capability to assign values outside of it's 
 ## calling environment.
 ## The function makeCacheMatix() generates a special matrix that can cache its inverse
@@ -8,11 +8,11 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
-    #set variable x (matrix invert in this case) to NULL
+    #set variable x (matrix in this case) to NULL
     x <- NULL
     
-    #set function sets x to the argument y and set m to null
-    #
+    ## set function sets x to the argument y and set m to null
+    
     set <- function(y) {
         x <<- y
         m <<- NULL
@@ -20,8 +20,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## cacheSolve() generates the inverse of a matrix x 
-## if the inverse exists and still valid it uses the cached version, otherwise it 
+## cacheSolve() generates the inverse of a matrix x.  If the inverse exists
+## and still valid, the function uses the cached version, otherwise it 
 ## calculates the invert using R's solve() function
 
 cacheSolve <- function(x, ...) {
@@ -32,8 +32,9 @@ cacheSolve <- function(x, ...) {
     x <- x$makeCacheMatrix()
     
     ## if not null, a valued was cached, so return x
+    
     if(!is.null(x)) {
-        message("getting cached data")
+        message("using cached data")
         return(x)
     }
     
